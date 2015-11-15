bd96640a3f3a26767ebaa3a39a24434a39addeec
Charles Hill <chill@degreesofzero.com>
2015-11-15 15:32:16 +0100

    Minor change to allow for ipv6 tests in future.

--- test/unit/checks/country.js
+++ test/unit/checks/country.js
@@ -8,28 +8,30 @@ var ProxyVerifier = require('../../../');
 
 describe('check.country(proxy)', function() {
 
-	var proxies = [
-		{
-			ip_address: '218.49.74.233',
-			country: 'kr'
-		},
-		{
-			ip_address: '200.150.97.27',
-			country: 'br'
-		},
-		{
-			ip_address: '198.169.246.30',
-			country: 'ca'
-		},
-		{
-			ip_address: '68.38.88.142',
-			country: 'us'
-		},
-		{
-			ip_address: '201.144.9.102',
-			country: 'mx'
-		}
-	];
+	var proxies = {
+		ipv4: [
+			{
+				ip_address: '218.49.74.233',
+				country: 'kr'
+			},
+			{
+				ip_address: '200.150.97.27',
+				country: 'br'
+			},
+			{
+				ip_address: '198.169.246.30',
+				country: 'ca'
+			},
+			{
+				ip_address: '68.38.88.142',
+				country: 'us'
+			},
+			{
+				ip_address: '201.144.9.102',
+				country: 'mx'
+			}
+		]
+	};
 
 	before(function(done) {
 
@@ -43,7 +45,7 @@ describe('check.country(proxy)', function() {
 
 	it('should give the correct country for proxies with IPv4 addresses', function() {
 
-		_.each(proxies, function(proxy) {
+		_.each(proxies.ipv4, function(proxy) {
 
 			var thrownError;
 
@@ -67,7 +69,7 @@ describe('check.country(proxy)', function() {
 			var i = 0;
 
 			var bench = new Benchmark(function() {
-				var proxy = proxies[i++] || proxies[i = 0];
+				var proxy = proxies.ipv4[i++] || proxies.ipv4[i = 0];
 				ProxyVerifier.check.country(proxy);
 			});
 
