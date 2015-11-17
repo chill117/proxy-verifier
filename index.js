183e6611c3ebbd553adbe66d455da0281a3b6d2c
Charles Hill <chill@degreesofzero.com>
2015-11-17 12:50:01 +0100

    Split out the geoip/country-lookup code into a separate module.

--- index.js
+++ index.js
@@ -3,18 +3,14 @@
 var _ = require('underscore');
 var async = require('async');
 var fs = require('fs');
+var GeoIpNativeLite = require('geoip-native-lite');
 var net = require('net');
 var ProxyAgent = require('proxy-agent');
 var request = require('request');
 var url = require('url');
 
-var utils = require('./lib/utils');
-
-var countryData = {};
-
 var ProxyVerifier = module.exports = {
 
-	_dataDir: __dirname + '/data',
 	_checkUrl: 'http://bitproxies.eu/api/v1/check',
 
 	/*
@@ -147,30 +143,7 @@ var ProxyVerifier = module.exports = {
 
 		country: function(proxy) {
 
-			var ipType = proxy.ip_address.indexOf(':') !== -1 ? 'ipv6': 'ipv4';
-			var list = countryData[ipType];
-
-			if (_.isUndefined(list)) {
-				throw new Error('Country data (' + ipType + ') has not been loaded.');
-			}
-
-			var ipToIntFn;
-
-			if (ipType === 'ipv6') {
-				ipToIntFn = utils.ipv6ToIntArray;
-			} else {
-				ipToIntFn = utils.ipv4ToInt;
-			}
-
-			var ipInt = ipToIntFn(proxy.ip_address);
-			var index = ProxyVerifier.binaryIpSearch(list, ipInt, ipType);
-
-			if (index === -1) {
-				// Not found.
-				return null;
-			}
-
-			return list[index].c;
+			return GeoIpNativeLite.lookup(proxy.ip_address);
 		}
 	},
 
@@ -258,151 +231,6 @@ var ProxyVerifier = module.exports = {
 
 	loadCountryData: function(options, cb) {
 
-		if (_.isUndefined(cb)) {
-			cb = options;
-			options = null;
-		}
-
-		options = _.defaults(options || {}, { ipv4: true, ipv6: false, cache: true });
-
-		async.parallel({
-
-			ipv4: function(next) {
-
-				if (!options.ipv4) {
-					return next();
-				}
-
-				if (options.cache && countryData.ipv4) {
-					return next(null, countryData.ipv4);
-				}
-
-				ProxyVerifier.loadCountryDataFromFile('ipv4', next);
-			},
-
-			ipv6: function(next) {
-
-				if (!options.ipv6) {
-					return next();
-				}
-
-				if (options.cache && countryData.ipv6) {
-					return next(null, countryData.ipv6);
-				}
-
-				ProxyVerifier.loadCountryDataFromFile('ipv6', next);
-			}
-
-		}, function(error, data) {
-
-			if (error) {
-				return cb(error);
-			}
-
-			if (options.cache) {
-				// Cache the data in memory.
-				_.each(data, function(_data, key) {
-					countryData[key] || (countryData[key] = _data);
-				});
-			}
-
-			cb(null, data);
-		});
-	},
-
-	loadCountryDataFromFile: function(ipType, cb) {
-
-		var dataFile = ProxyVerifier._dataDir + '/country-' + ipType + '.json';
-
-		fs.readFile(dataFile, 'utf8', function(error, data) {
-
-			if (error) {
-				return cb(error);
-			}
-
-			try {
-				data = JSON.parse(data);
-			} catch (error) {
-				return cb(error);
-			}
-
-			cb(null, data);
-		});
-	},
-
-	/*
-		Similar to underscore's sortedIndex(), but with a comparator function.
-
-		And a little bit more efficient for our needs.
-	*/
-	binaryIpSearch: function(ipRangesArray, ip, type) {
-
-		var comparator = type === 'ipv6' ? ProxyVerifier.inRangeIpv6 : ProxyVerifier.inRangeIpv4;
-		var low = 0;
-		var high = ipRangesArray.length;
-		var mid;
-		var result;
-
-		while (low < high) {
-
-			mid = Math.floor((low + high) / 2);
-			result = comparator(ipRangesArray[mid], ip);
-
-			if (result === 0) {
-				return mid;
-			}
-
-			if (result === -1) {
-				low = mid + 1;
-			} else {
-				high = mid;
-			}
-		}
-
-		return -1;
-	},
-
-	inRangeIpv4: function(ipRange, ip) {
-
-		var start = ipRange.s;
-		var end = ipRange.e || ipRange.s;
-
-		return ip < start ? 1 : ip > end ? -1 : 0;
-	},
-
-	inRangeIpv6: function(ipRange, ip) {
-
-		var start = ipRange.s;
-		var end = ipRange.e;
-
-		if (!end) {
-			return ProxyVerifier.compareIpv6(start, ip);
-		}
-
-		if (ProxyVerifier.compareIpv6(start, ip) === 1) {
-			return 1;
-		}
-
-		if (ProxyVerifier.compareIpv6(end, ip) === -1) {
-			return -1;
-		}
-
-		return 0;
-	},
-
-	compareIpv6: function(ip1, ip2) {
-
-		for (var i = 0; i < 2; i++) {
-
-			if (ip1[i] < ip2[i]) {
-				return -1;
-			}
-
-			if (ip1[i] > ip2[i]) {
-				return 1;
-			}
-		}
-
-		return 0;
+		GeoIpNativeLite.loadData(options, cb);
 	}
 };
--- readme.md
+++ readme.md
@@ -3,6 +3,3 @@
 Check that proxies are working, verify their anonymity level and country.
 
 [![Build Status](https://travis-ci.org/chill117/proxy-verifier.svg?branch=master)](https://travis-ci.org/chill117/proxy-verifier) [![Status of Dependencies](https://david-dm.org/chill117/proxy-verifier.svg)](https://david-dm.org/chill117/proxy-verifier)
-
-
-
